25a372063bddfa00e35536ecd1332f75ccc194c3
altinaD <71178866+altinaD@users.noreply.github.com>
2023-01-19 15:49:17 +0500

    Add files via upload

---
+++ self-made-scroll-header.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <header :class="{ hide: isHidden }">I am a header</header>
+    <section></section>
+    <footer></footer>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isHidden: false,
+      temp: 0,
+      scrollY: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.scrollHeader);
+  },
+  methods: {
+    scrollHeader() {
+      this.scrollY = window.scrollY;
+      if (this.scrollY == 0) {
+        this.isHidden = false;
+      }
+      if (this.scrollY > this.temp) {
+        // scrolled down
+        this.isHidden = true;
+      } else if (this.scrollY < this.temp) {
+        // scrolled up
+        this.isHidden = false;
+      }
+      this.temp = this.scrollY;
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 20vh;
+  background-color: tomato;
+  transform: translateY(0);
+  position: fixed;
+  transition: transform 0.3s ease;
+}
+.hide {
+  transform: translateY(-20vh);
+}
+section {
+  min-height: 200vh;
+  height: 100%;
+  width: 100%;
+}
+</style>
